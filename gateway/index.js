5fa92a12e161255f15c0fa0319acfaf92e86dfd0
faaiz <email>
2024-01-26 23:52:36 +0500

    working on micro services architecture

--- gateway/index.js
+++ gateway/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const gateway = require("fast-gateway");
 const dotenv=require("dotenv")
 dotenv.config()
 const os=require("os")
@@ -14,17 +15,46 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-
-
-
-app.get("/", (req, res) => {
-  return res.send({
-    status: 200,
-    message: `You are interacted with admin Micro services at ${process.env.PORT} ${os.hostname()}`,
-  });
+const server = gateway({
+  routes: [
+    {
+      prefix: "/admin",
+      target: "http://localhost:3001/",
+    },
+    {
+      prefix: "/driver",
+      target: "http://driver:3002/",
+    },
+    {
+      prefix: "/user",
+      target: "http://driver:3003/",
+    },
+    {
+      prefix: "/patient",
+      target: "http://driver:3004/",
+    },
+    {
+      prefix: "/ambulance",
+      target: "http://driver:3005/",
+    },
+    {
+      prefix: "/ride",
+      target: "http://driver:3006/",
+    },
+  ],
 });
 
 
+server.get("/", (req, res) => {
+  return res.send({
+    status: 200,
+    message: `You are interacted with Micro services at ${process.env.PORT} ${os.hostname()}`,
+  });
+});
+
+server.start(process.env.port).then((server) => {
+  console.log(`Gateway is connected at ${process.env.port}`);
+});
 
 // app.get("/", (req, res) => {
 //   res.send("Welcome to FHCS Ambulance Service");
@@ -40,7 +70,7 @@ app.get("/", (req, res) => {
 // app.use("/api/fhcsambulance/patient",patientR)
 // app.use("/api/fhcsambulance/allocation",ambulanceAllocateToDriverR)
 
-app.listen(process.env.PORT, () => {
-  console.log("server is running");
-});
+// app.listen(process.env.PORT, () => {
+//   console.log("server is running");
+// });
 
